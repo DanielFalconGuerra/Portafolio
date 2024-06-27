0c5b6d6401c0581dee674633579b4f858d84ee53
DanielFalcon <danielfalcon.87@gmail.com>
2024-06-27 15:54:53 -0600

    SportsAPI added

---
+++ Portafolio/Controllers/SportsAPIController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Portafolio.Controllers
+{
+    public class SportsAPIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
